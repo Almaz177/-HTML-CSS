d719a5e12045965b31811062dc947a12bcb2f9ce
Almaz177 <116495709+Almaz177@users.noreply.github.com>
2022-10-23 19:49:10 +0500

    Add files via upload

---
+++ test-1.js
@@ -0,0 +1,29 @@
+<div class="quizsection">
+  
+    <h2>Вопрос #1</h2>
+     
+    <div class="answer">
+        <input name="q1" value="value2" id="value1" type="radio" checked="checked" /> Value 1
+    </div>
+  
+    <div class="answer">
+        <input name="q1" value="value1" id="value2" type="radio" /> Value 2
+    </div>
+  
+    <div class="answer">
+        <input name="q1" value="value1" id="value3" type="radio" /> Value 3
+    </div>
+  
+    <div class="answer">
+        <input name="q1" value="value1" id="value4" type="radio" /> Value 4
+    </div>
+  
+    <div class="answer">
+        <input name="q1" value="value1" id="value5" type="radio" /> Value 5
+    </div>
+  
+    <div class="answer">
+        <input name="q1" value="value1" id="value6" type="radio" /> Value 6
+    </div>
+  
+</div>
